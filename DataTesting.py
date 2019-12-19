40d8082a2f65d58f45e112202463274f7ce9bce6
James Garay <jmgaray11@gmail.com>
2019-12-19 12:53:20 -0500

    updates

--- DataTesting.py
+++ DataTesting.py
@@ -1,19 +1,28 @@
 import json
+import googlemaps
+from datetime import datetime
 
-fname = './zipcodeGeoJSON/ri_rhode_island_zip_codes_geo.min.json'
-fhand = open(fname, 'r+')
+gmaps = googlemaps.Client(key='AIzaSyCjoOrgY8JDznjT9pVauI62XkVwVLL_nIQ')
 
-json1 = json.load(fhand)
-count = 0
-mapper = dict()
+geocode_result = gmaps.geocode('9630 1.5 Mile Road, East Leroy, MI 49051')
 
-for obj in json1['features']:
-    print(obj['properties']['ZCTA5CE10'])
-    mapper[obj['properties']['ZCTA5CE10']] = count % 10
-    count += 1
-    obj['properties']['funds'] = count % 10
+print(geocode_result)
 
-json.dump(json1, fhand)
+
+# fname = './zipcodeGeoJSON/ri_rhode_island_zip_codes_geo.min.json'
+# fhand = open(fname, 'r+')
+#
+# json1 = json.load(fhand)
+# count = 0
+# mapper = dict()
+#
+# for obj in json1['features']:
+#     print(obj['properties']['ZCTA5CE10'])
+#     mapper[obj['properties']['ZCTA5CE10']] = count % 10
+#     count += 1
+#     obj['properties']['funds'] = count % 10
+#
+# json.dump(json1, fhand)
 
 #
 # print(count)
@@ -28,3 +37,5 @@ json.dump(json1, fhand)
 # json2 = json.dump(obj1, fhand2)
 #
 # fhand2.close()
+
+
