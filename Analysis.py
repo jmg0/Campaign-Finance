c60e924b8caeee0de2a59aa0f05a8b2fcee359e2
James Garay <jmgaray11@gmail.com>
2019-12-23 11:29:26 -0500

    database reads from FEC csv files and adds contributions to appropriate candidate relation. Need to address some contribution numbers returning as BLOB rather than number

--- Analysis.py
+++ Analysis.py
@@ -10,26 +10,41 @@ def arrange_dataframe(dataframe):
     contributor_occupation = dataframe['contributor_occupation']
     contributor_date = dataframe['contribution_receipt_date']
     contributor_contribution = dataframe['contribution_receipt_amount']
-    contributor_address = list()
+    contributor_address = dict()
     for i in range(len(contributor_street_address)):
-        contributor_address[i] = contributor_street_address + ', ' + contributor_city + ', ' + contributor_state + " " + contributor_zip
+        contributor_address[i] = str(contributor_street_address[i]) + ', ' + str(contributor_city[i]) + ', ' + str(contributor_state[i]) + ' ' + str(contributor_zip[i])
     return [contributor_names, contributor_address, contributor_contribution, contributor_date, contributor_occupation]
 
 
 def candidate_database_analysis(cursor, candidate_name):
-    fname = './Finance Data/' + candidate_name + '_raw_contribution_data.csv'
+    fname = './Finance Data/' + candidate_name + '/' + candidate_name + '_raw_contribution_data.csv'
     dataframe = panda.read_csv(fname)
     contributor_info = arrange_dataframe(dataframe)
 
     relation_name = candidate_name + '_Contributions'
     cursor.execute('''CREATE TABLE IF NOT EXISTS ''' + relation_name + '''
                     (id INTEGER UNIQUE, 
-                    Name TEXT, Address TEXT, Contribution REAL, Date TEXT, Occupation TEXT)''')
+                    Name TEXT, Address TEXT, Contribution INTEGER, Date TEXT, Occupation TEXT)''')
+
+    start = None
+    cursor.execute('SELECT max(id) FROM ' + relation_name)
+    try:
+        row = cursor.fetchone()
+        if row is None:
+            start = 0
+        else:
+            start = row[0]
+    except:
+        start = 0
+
+    if start is None:
+        start = 0
 
     for i in range(len(contributor_info[0])):
         cursor.execute('''INSERT OR IGNORE INTO ''' + relation_name + '''
                     (id, Name, Address, Contribution, Date, Occupation) VALUES
                     ( ?, ?, ?, ?, ?, ?)''', (start, contributor_info[0][i], contributor_info[1][i], contributor_info[2][i], contributor_info[3][i], contributor_info[4][i]) )
+        start += 1
 
 
 
@@ -37,15 +52,15 @@ def candidate_database_analysis(cursor, candidate_name):
 def main():
     database_name = 'raw_contribution_data.sqlite'
     connector = sqlite3.connect(database_name)
-    cursor = connector.cursor()
+
 
     candidate_names = ['Trump', 'Sanders', 'Warren', 'Buttigieg', 'Biden', 'Klobuchar', 'Yang']
     for candidate in candidate_names:
+        cursor = connector.cursor()
         candidate_database_analysis(cursor, candidate)
-        #print(candidate)
+        connector.commit()
+        cursor.close()
 
-    connector.commit()
-    cursor.close()
 
 if __name__  == '__main__':
     main()
