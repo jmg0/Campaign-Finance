2702cb24d46986fd678143663d850c50e3b316b1
James Garay <jmgaray11@gmail.com>
2019-12-23 16:23:55 -0500

    fixed BLOB issue (forced data cast before loading), created primary key based on name/address which is YUGE-ly important

--- Analysis.py
+++ Analysis.py
@@ -16,15 +16,15 @@ def arrange_dataframe(dataframe):
     return [contributor_names, contributor_address, contributor_contribution, contributor_date, contributor_occupation]
 
 
-def candidate_database_analysis(cursor, candidate_name):
+def candidate_database_populate(cursor, candidate_name):
     fname = './Finance Data/' + candidate_name + '/' + candidate_name + '_raw_contribution_data.csv'
     dataframe = panda.read_csv(fname)
     contributor_info = arrange_dataframe(dataframe)
 
     relation_name = candidate_name + '_Contributions'
     cursor.execute('''CREATE TABLE IF NOT EXISTS ''' + relation_name + '''
-                    (id INTEGER UNIQUE, 
-                    Name TEXT, Address TEXT, Contribution INTEGER, Date TEXT, Occupation TEXT)''')
+                    (Contributor_id INTEGER, id INTEGER UNIQUE,
+                    Name TEXT, Address TEXT, Contribution NUMERIC, Date TEXT, Occupation TEXT)''')
 
     start = None
     cursor.execute('SELECT max(id) FROM ' + relation_name)
@@ -40,11 +40,32 @@ def candidate_database_analysis(cursor, candidate_name):
     if start is None:
         start = 0
 
+    contributor_id = 0
     for i in range(len(contributor_info[0])):
+        cursor.execute('SELECT * FROM ' + relation_name + ' WHERE Name=? AND Address=?', (contributor_info[0][i], contributor_info[1][i]))
+        try:
+            row = cursor.fetchone()
+            if row is not None:
+                contributor_id = row[0]
+            else:
+                contributor_id += 1
+        except:
+            contributor_id += 1
+
         cursor.execute('''INSERT OR IGNORE INTO ''' + relation_name + '''
-                    (id, Name, Address, Contribution, Date, Occupation) VALUES
-                    ( ?, ?, ?, ?, ?, ?)''', (start, contributor_info[0][i], contributor_info[1][i], contributor_info[2][i], contributor_info[3][i], contributor_info[4][i]) )
+                    (Contributor_id, id, Name, Address, Contribution, Date, Occupation) VALUES
+                    ( ?, ?, ?, ?, ?, ?, ?)''', (contributor_id, start, contributor_info[0][i], contributor_info[1][i], float(contributor_info[2][i]), contributor_info[3][i], contributor_info[4][i]) )
         start += 1
+    return
+
+# def candidate_database_compress(cursor, candidate_name):
+#     contributor_map = dict()
+#     relation_name = candidate_name + '_Contributions'
+#     compressed_relation_name = candidate_name + '_Contributions_compressed'
+#
+#     cursor.execute('SELECT * FROM ' + relation_name)
+#     for row in cursor:
+
 
 
 
@@ -57,7 +78,7 @@ def main():
     candidate_names = ['Trump', 'Sanders', 'Warren', 'Buttigieg', 'Biden', 'Klobuchar', 'Yang']
     for candidate in candidate_names:
         cursor = connector.cursor()
-        candidate_database_analysis(cursor, candidate)
+        candidate_database_populate(cursor, candidate)
         connector.commit()
         cursor.close()
 
---
+++ Contributor.py
@@ -0,0 +1,9 @@
+
+
+class Contributor:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.contribution_total = 0
+        self.num_contributions = 0
+
