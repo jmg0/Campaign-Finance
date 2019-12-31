95023d211de572543d3bb8b77ce30e0dbf8241b9
James Garay <jmgaray11@gmail.com>
2019-12-31 12:52:47 -0500

    adding geocoding functionality to get around rate lims

--- Geocoder.py
+++ Geocoder.py
@@ -1,13 +1,27 @@
 import geocoder
 import re
 import sqlite3
+import googlemaps
+import Hidden
+import json
 
-def geocode_addresses(address):
+def geocode_addresses_osm(address):
     g = geocoder.osm(address)
     if g.latlng is None:
         address = re.findall('.*, (.*, .*)', address)[0]
         g = geocoder.osm(address)
+        if g.latlng is None:
+            address = re.findall('.*, (.*)', address)
+            g = geocoder.osm(address)
     return g.latlng
+
+def geocode_addresses_google(address):
+    gmaps = googlemaps.Client(key=Hidden.google_api_key)
+    g_json = gmaps.geocode(address)
+    g = g_json[0]
+    lat = g['geometry']['location']['lat']
+    lng = g['geometry']['location']['lng']
+    return [lat, lng]
 
 def geocode_database(database_name, candidate_name):
     try:
