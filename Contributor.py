2bcd64e1eeaba6dc490a6d2eec8598dbb8c9d59d
James Garay <48111471+jmg0@users.noreply.github.com>
2020-01-29 13:26:08 -0500

    Delete Contributor.py

--- Contributor.py
+++
@@ -1,9 +0,0 @@
-
-
-class Contributor:
-    def __init__(self, name, address):
-        self.name = name
-        self.address = address
-        self.contribution_total = 0
-        self.num_contributions = 0
-
